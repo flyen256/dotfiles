85e862f827ba2c89fa0fbf7fea02e7ea3279b996
flyen256 <mak2021lolol@gmail.com>
2026-07-29 00:06:31 +0500

    added: README.md, modified install dependencies script

--- hypr/install_dependencies.sh
+++ hypr/install_dependencies.sh
@@ -1,6 +1,3 @@
 sudo pacman -Sy hyprland gammastep hyprlock hyprshot hyprpaper hyprpicker hypridle zen-browser-bin pavucontrol telegram-desktop discord nautilus ttf-jetbrains-mono-nerd
 yay -S aylurs-gtk-shell-git libastal apple_cursor
 ags types
-git clone https://github.com/flyen256/ags-dotfiles ~/.config/.ags
-mv ~/.config/.ags/{.,}* ~/.config/ags
-rm -rf ~/.config/.ags
