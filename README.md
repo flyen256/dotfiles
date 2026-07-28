9706b4848f61cf58e8b9accd28de63a4978485ff
flyen256 <mak2021lolol@gmail.com>
2026-07-29 00:14:01 +0500

    modified: README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
+# Powered by
+
+- Window manager >> [Hyprland <3](https://github.com/hyprwm/hyprland)
+- Terminal >> [kitty](https://github.com/kovidgoyal/kitty)
+- App launcher >> [rofi](https://github.com/davatorium/rofi)
+- Shell >> [ags](https://github.com/aylur/ags)
+
 # Settings
 
-> Icon theme - Adwaita
-> Cursor - apple_cursor (yay -S apple_cursor)
+- Icon theme - Adwaita
+- Cursor - apple_cursor (yay -S apple_cursor)
--- hypr/install_dependencies.sh
+++ hypr/install_dependencies.sh
@@ -1,3 +1,3 @@
-sudo pacman -Sy hyprland gammastep hyprlock hyprshot hyprpaper hyprpicker hypridle zen-browser-bin pavucontrol telegram-desktop discord nautilus ttf-jetbrains-mono-nerd
+sudo pacman -Sy hyprland gammastep hyprlock hyprshot hyprpaper hyprpicker hypridle zen-browser-bin pavucontrol telegram-desktop discord nautilus ttf-jetbrains-mono-nerd ttf-material-symbols-variable
 yay -S aylurs-gtk-shell-git libastal apple_cursor
 ags types
