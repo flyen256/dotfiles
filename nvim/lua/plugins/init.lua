274004bc2d4d451d2b1316e09544df25a0d86e73
flyen256 <mak2021lolol@gmail.com>
2026-07-29 03:15:14 +0500

    migrated neovim to adwaita theme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -364,8 +364,21 @@ return {
 	{
 		"ellisonleao/gruvbox.nvim",
 		config = function()
-			vim.cmd("colorscheme gruvbox")
+			--			vim.cmd("colorscheme gruvbox")
 		end
+	},
+	{
+			"Mofiqul/adwaita.nvim",
+			lazy = false,
+			priority = 1000,
+			
+			-- configure and set on startup
+			config = function()
+					vim.g.adwaita_darker = true
+					vim.g.adwaita_disable_cursorline = true
+					vim.g.adwaita_transparent = true
+					vim.cmd('colorscheme adwaita')
+			end
 	},
 	{
 		"apyra/nvim-unity-sync",
