a271cfc9094f17fb8eea5c12791a5dc701cd4fa6
flyen256 <lolka1337kamlol@gmail.com>
2026-08-10 17:15:36 +0500

    fix: restart ags script

--- hypr/scripts/restart_ags.sh
+++ hypr/scripts/restart_ags.sh
@@ -4,9 +4,10 @@ MEM_SCRIPT="/home/$USER/.config/ags/scripts/mem.sh"
 CPU_SCRIPT="/home/$USER/.config/ags/scripts/cpu.sh"
 GPU_SCRIPT="/home/$USER/.config/ags/scripts/gpu.sh"
 
+ags quit
+
 if pidof ags > /dev/null; then
   echo "ags is running. Restarting..."
-  ags quit
 	pkill -f "sh $MEM_SCRIPT"
 	pkill -f "sh $CPU_SCRIPT"
 	pkill -f "sh $GPU_SCRIPT"
